b5fedd9e02d80ce02229eeaf3f103b5b09876568
Sym Conger <acesilverryuu@gmail.com>
2019-04-09 12:44:16 -0500

    updated comment

--- src/TableAndChairs.java
+++ src/TableAndChairs.java
@@ -19,6 +19,8 @@ public class TableAndChairs {
         System.out.println("I        I       G                  G        I        I");
         System.out.println("R        R       !                  !        R        R");
 
+        System.out.println("Hello people.");
+
     }
 
 }
