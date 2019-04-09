29b3ce0f92b3d521165159ade929c7a18a3bc008
Sym Conger <acesilverryuu@gmail.com>
2019-04-09 13:17:57 -0500

    made a taco truck

---
+++ src/TacoTruck.java
@@ -0,0 +1,27 @@
+public class TacoTruck {
+    //Author:Sym
+    //Date: 04/09/2019
+    //Project: Taco Truck
+
+    public static void main(String[] args) {
+        System.out.println("Building A Taco Truck");
+        System.out.println("/n/n/n/n");
+
+        System.out.println("----------------------------------------------");
+        System.out.println("l        Welcome To Delgados Tacos!          l");
+        System.out.println("l      1____________________________1        l");
+        System.out.println("l      1                            1        l");
+        System.out.println("l      1                            1        l - l-------------l                 ");
+        System.out.println("l      1                            l        l - l     l-----l  l                ");
+        System.out.println("l      1                            1        l - l     l     l   l               ");
+        System.out.println("l      1                            1        l - l     l______l___l              ");
+        System.out.println("l      1____________________________1        l - l     l _     l  l              ");
+        System.out.println("l                                            l - l     l       l  l              ");
+        System.out.println("l                                            l - l     l-------l  l              ");
+        System.out.println("l     oooo                       oooo        l - l   oooo         l              ");
+        System.out.println("-----oooooo---------------------oooooo--------   l--oooooo---------              ");
+        System.out.println("      oooo                       oooo                oooo");
+
+    }
+
+}
