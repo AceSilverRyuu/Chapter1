5ef81147239844f6e7cad0acd56e5dd37b3a0f92
Sym Conger <acesilverryuu@gmail.com>
2019-04-09 12:35:17 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,24 @@
+public class TableAndChairs {
+//Author:Sym
+    //Date: 04/09/2019
+    //Project: Tables And Chairs
+
+    public static void main(String[] args) {
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("/n/n/n/n");
+
+        System.out.println("C                                                     C");
+        System.out.println("H                                                     H");
+        System.out.println("A                                                     A");
+        System.out.println("I             TABLETABLETABLETABLETABLE               I");
+        System.out.println("R                L                  L                 R");
+        System.out.println("CHAIRCHAIR       E                  E        CHAIRCHAIR");
+        System.out.println("C        C       G                  G        C        C");
+        System.out.println("H        H       L                  L        H        H");
+        System.out.println("A        A       E                  E        A        A");
+        System.out.println("I        I       G                  G        I        I");
+        System.out.println("R        R       !                  !        R        R");
+
+    }
+
+}
